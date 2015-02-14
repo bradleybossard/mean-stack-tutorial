758546ba129545864647f73b6cebdf89e8be8042
Bradley Bossard <bradleybossard@gmail.com>
2015-02-13 16:12:42 -0800

    Added a form for adding a custom title to the list, add logic to prevent blank field from being submitted.

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ app.controller('MainCtrl', [ '$scope', function($scope){
   ];
 
   $scope.addPost = function(){
-    $scope.posts.push({title: 'A new post!', upvotes: 0});
+    if(!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
   };
 
 
